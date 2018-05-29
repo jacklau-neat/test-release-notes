c68de403afa350eb159b80b4e30bc64ba098c13f
Jack Lau <jackpocket@gmail.com>
2018-05-29 14:39:07 +0800

    no message

--- script/generate_changelog.sh
+++ script/generate_changelog.sh
@@ -2,26 +2,18 @@
 TAG_VER=$1
 
 # Generate the CHANGELOG based on the tags
-# in order to add the CHANGELOG within the tag too, delete and tag it again after commit the CHANGELOG
-function git_tag {
-  git tag "$TAG_VER"
-  git push --tags
-}
-
-function remove_git_tag {
-  git push --delete origin "$TAG_VER"
-  git tag --delete "$TAG_VER"
-}
-
-git_tag
-
 gren changelog --generate --override --tags=all
 
-remove_git_tag
-
+# Since master is a protected branch, it commit to changelog branch and create a pull request
+git checkout -B changelog
 git add CHANGELOG.md
-git commit -m "Update CHANGELOG, Release $TAG_VER"
+git commit -m "Update CHANGELOG for $TAG_VER"
 git push
 
-git_tag
+# Craete pull request to merge the changelog into master
+hub pull-request -m "Update CHANGELOG for $TAG_VER" -b master
+
+git checkout master
+
+# Generate GitHub release notes
 gren release --tags=$TAG_VER
--- script/tag.sh
+++ script/tag.sh
@@ -1,4 +1,6 @@
 #!/bin/env bash
 NEW_TAG=$1
 echo "Tagging $NEW_TAG"
+git tag "$NEW_TAG"
+git push --tags
 bash ./script/generate_changelog.sh "$NEW_TAG"
